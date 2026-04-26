0fb466ca782ed0c10961b89e817974bb6fbf7dcc
Sibghatullah <145217999+sibu876@users.noreply.github.com>
2026-04-26 22:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const number = document.getElementById("number");
+const inc = document.getElementById("increment");
+
+let count =0;
+const  increment = () => {
+	count++;
+	number.textContent = count;
+	
+} 
+inc.onclick = increment;
